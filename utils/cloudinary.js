8e3e6917d3980a8433214010c015ec63bb216490
Tijania Williams <vtwilliams@ptgrouphotels.com>
2025-05-23 12:21:33 -0700

    new changes

---
+++ utils/cloudinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.dnbhxjtr7,
+  api_key: process.env.936663371467975,
+  api_secret: process.env.pY4yesnZRnkj66Z0L0QCGWxUPWo,
+});
+
+export default cloudinary;
